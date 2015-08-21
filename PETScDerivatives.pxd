223626b872811a3ce95f3968d4ac403ffabf5762
Michael Kraus <michael.kraus@ipp.mpg.de>
2015-08-21 12:32:27 +0200

    Cleanup in derivatives.

--- PETScDerivatives.pxd
+++ PETScDerivatives.pxd
@@ -33,32 +33,17 @@ cdef class PETScDerivatives(object):
     cdef np.ndarray ty
     
     
-    cdef np.float64_t arakawa(self, np.ndarray[np.float64_t, ndim=2] x,
-                                    np.ndarray[np.float64_t, ndim=2] h,
-                                    np.uint64_t i, np.uint64_t j)
+    cdef double arakawa(self, double[:,:] x, double[:,:] h,
+                              np.uint64_t i, np.uint64_t j)
 
-    cdef np.float64_t arakawa_grad(self, np.ndarray[np.float64_t, ndim=2] x,
-                                         np.ndarray[np.float64_t, ndim=2] h,
-                                         np.uint64_t i, np.uint64_t j)
-
-    cdef np.float64_t laplace(self, np.ndarray[np.float64_t, ndim=2] x,
-                                    np.uint64_t i, np.uint64_t j)
-    
-    cdef np.float64_t laplace_ave(self, np.ndarray[np.float64_t, ndim=2] x,
-                                        np.uint64_t i, np.uint64_t j)
+    cdef double laplace(self, double[:,:] x,
+                              np.uint64_t i, np.uint64_t j)
     
     cpdef laplace_vec(self, Vec X, Vec D, np.float64_t sign)
     
-    cpdef np.float64_t dx(self, Vec X, Vec D, np.float64_t sign)
+    cpdef double dx(self, Vec X, Vec D, np.float64_t sign)
     
-    cpdef np.float64_t dy(self, Vec X, Vec D, np.float64_t sign)
+    cpdef double dy(self, Vec X, Vec D, np.float64_t sign)
 
-    cdef np.float64_t dt(self, np.ndarray[np.float64_t, ndim=2] x,
-                               np.uint64_t i, np.uint64_t j)
-
-    cdef np.float64_t average(self, np.ndarray[np.float64_t, ndim=2] x,
-                                    np.uint64_t i, np.uint64_t j)
-
-    cdef np.float64_t dt_laplace(self, np.ndarray[np.float64_t, ndim=2] x,
-                                       np.uint64_t i, np.uint64_t j)
+    cdef  double dt(self, double[:,:] x, np.uint64_t i, np.uint64_t j)
 
--- PETScDerivatives.pyx
+++ PETScDerivatives.pyx
@@ -49,9 +49,8 @@ cdef class PETScDerivatives(object):
         
     
     @cython.boundscheck(False)
-    cdef np.float64_t arakawa(self, np.ndarray[np.float64_t, ndim=2] x,
-                                    np.ndarray[np.float64_t, ndim=2] h,
-                                    np.uint64_t i, np.uint64_t j):
+    cdef double arakawa(self, double[:,:] x, double[:,:] h,
+                              np.uint64_t i, np.uint64_t j):
         '''
         MHD Derivative: Arakawa Bracket
         '''
@@ -76,178 +75,9 @@ cdef class PETScDerivatives(object):
         return result
     
     
-    @cython.boundscheck(False)
-    cdef np.float64_t arakawa_grad(self, np.ndarray[np.float64_t, ndim=2] x,
-                                         np.ndarray[np.float64_t, ndim=2] h,
-                                         np.uint64_t i, np.uint64_t j):
-        '''
-        MHD Derivative: Arakawa Bracket
-        '''
-        
-        cdef np.float64_t result
-        
-        result = ( \
-                     + 0.5 * x[i-2, j-1] * h[i-1, j-1] \
-                     + 0.5 * x[i-2, j-1] * h[i-1, j+1] \
-                     - 1.  * x[i-2, j-1] * h[i-1, j  ] \
-                     - 0.5 * x[i-2, j+1] * h[i-1, j-1] \
-                     - 0.5 * x[i-2, j+1] * h[i-1, j+1] \
-                     + 1.  * x[i-2, j+1] * h[i-1, j  ] \
-                     + 1.  * x[i-1, j-2] * h[i,   j-2] \
-                     - 1.  * x[i-1, j-2] * h[i,   j-1] \
-                     - 0.5 * x[i-1, j-1] * h[i-2, j-1] \
-                     + 0.5 * x[i-1, j-1] * h[i-2, j+1] \
-                     + 4.  * x[i-1, j-1] * h[i-1, j+1] \
-                     - 4.  * x[i-1, j-1] * h[i-1, j  ] \
-                     + 0.5 * x[i-1, j-1] * h[i,   j-1] \
-                     - 0.5 * x[i-1, j-1] * h[i,   j+1] \
-                     - 0.5 * x[i-1, j+1] * h[i-2, j-1] \
-                     + 0.5 * x[i-1, j+1] * h[i-2, j+1] \
-                     - 4.  * x[i-1, j+1] * h[i-1, j-1] \
-                     + 4.  * x[i-1, j+1] * h[i-1, j  ] \
-                     + 0.5 * x[i-1, j+1] * h[i,   j-1] \
-                     - 0.5 * x[i-1, j+1] * h[i,   j+1] \
-                     + 1.  * x[i-1, j+2] * h[i,   j+1] \
-                     - 1.  * x[i-1, j+2] * h[i,   j+2] \
-                     + 1.  * x[i-1, j  ] * h[i-2, j-1] \
-                     - 1.  * x[i-1, j  ] * h[i-2, j+1] \
-                     + 4.  * x[i-1, j  ] * h[i-1, j-1] \
-                     - 4.  * x[i-1, j  ] * h[i-1, j+1] \
-                     - 1.  * x[i-1, j  ] * h[i,   j-2] \
-                     + 1.  * x[i-1, j  ] * h[i,   j+2] \
-                     - 1.  * x[i+1, j-2] * h[i,   j-2] \
-                     + 1.  * x[i+1, j-2] * h[i,   j-1] \
-                     - 4.  * x[i+1, j-1] * h[i+1, j+1] \
-                     + 4.  * x[i+1, j-1] * h[i+1, j  ] \
-                     + 0.5 * x[i+1, j-1] * h[i+2, j-1] \
-                     - 0.5 * x[i+1, j-1] * h[i+2, j+1] \
-                     - 0.5 * x[i+1, j-1] * h[i,   j-1] \
-                     + 0.5 * x[i+1, j-1] * h[i,   j+1] \
-                     + 4.  * x[i+1, j+1] * h[i+1, j-1] \
-                     - 4.  * x[i+1, j+1] * h[i+1, j  ] \
-                     + 0.5 * x[i+1, j+1] * h[i+2, j-1] \
-                     - 0.5 * x[i+1, j+1] * h[i+2, j+1] \
-                     - 0.5 * x[i+1, j+1] * h[i,   j-1] \
-                     + 0.5 * x[i+1, j+1] * h[i,   j+1] \
-                     - 1.  * x[i+1, j+2] * h[i,   j+1] \
-                     + 1.  * x[i+1, j+2] * h[i,   j+2] \
-                     - 4.  * x[i+1, j  ] * h[i+1, j-1] \
-                     + 4.  * x[i+1, j  ] * h[i+1, j+1] \
-                     - 1.  * x[i+1, j  ] * h[i+2, j-1] \
-                     + 1.  * x[i+1, j  ] * h[i+2, j+1] \
-                     + 1.  * x[i+1, j  ] * h[i,   j-2] \
-                     - 1.  * x[i+1, j  ] * h[i,   j+2] \
-                     - 0.5 * x[i+2, j-1] * h[i+1, j-1] \
-                     - 0.5 * x[i+2, j-1] * h[i+1, j+1] \
-                     + 1.  * x[i+2, j-1] * h[i+1, j  ] \
-                     + 0.5 * x[i+2, j+1] * h[i+1, j-1] \
-                     + 0.5 * x[i+2, j+1] * h[i+1, j+1] \
-                     - 1.  * x[i+2, j+1] * h[i+1, j  ] \
-                     - 1.  * x[i,   j-2] * h[i-1, j-2] \
-                     + 1.  * x[i,   j-2] * h[i-1, j  ] \
-                     + 1.  * x[i,   j-2] * h[i+1, j-2] \
-                     - 1.  * x[i,   j-2] * h[i+1, j  ] \
-                     + 1.  * x[i,   j-1] * h[i-1, j-2] \
-                     - 0.5 * x[i,   j-1] * h[i-1, j-1] \
-                     - 0.5 * x[i,   j-1] * h[i-1, j+1] \
-                     - 1.  * x[i,   j-1] * h[i+1, j-2] \
-                     + 0.5 * x[i,   j-1] * h[i+1, j-1] \
-                     + 0.5 * x[i,   j-1] * h[i+1, j+1] \
-                     + 0.5 * x[i,   j+1] * h[i-1, j-1] \
-                     + 0.5 * x[i,   j+1] * h[i-1, j+1] \
-                     - 1.  * x[i,   j+1] * h[i-1, j+2] \
-                     - 0.5 * x[i,   j+1] * h[i+1, j-1] \
-                     - 0.5 * x[i,   j+1] * h[i+1, j+1] \
-                     + 1.  * x[i,   j+1] * h[i+1, j+2] \
-                     + 1.  * x[i,   j+2] * h[i-1, j+2] \
-                     - 1.  * x[i,   j+2] * h[i-1, j  ] \
-                     - 1.  * x[i,   j+2] * h[i+1, j+2] \
-                     + 1.  * x[i,   j+2] * h[i+1, j  ] \
-                 ) * self.hx_inv * self.hy_inv**3 / 24. \
-               + ( \
-                     - 1.  * x[i-2, j-1] * h[i-2, j  ] \
-                     + 1.  * x[i-2, j-1] * h[i-1, j  ] \
-                     + 1.  * x[i-2, j+1] * h[i-2, j  ] \
-                     - 1.  * x[i-2, j+1] * h[i-1, j  ] \
-                     + 1.  * x[i-2, j  ] * h[i-2, j-1] \
-                     - 1.  * x[i-2, j  ] * h[i-2, j+1] \
-                     - 1.  * x[i-2, j  ] * h[i,   j-1] \
-                     + 1.  * x[i-2, j  ] * h[i,   j+1] \
-                     - 0.5 * x[i-1, j-2] * h[i-1, j-1] \
-                     - 0.5 * x[i-1, j-2] * h[i+1, j-1] \
-                     + 1.  * x[i-1, j-2] * h[i,   j-1] \
-                     + 0.5 * x[i-1, j-1] * h[i-1, j-2] \
-                     - 0.5 * x[i-1, j-1] * h[i-1, j  ] \
-                     - 0.5 * x[i-1, j-1] * h[i+1, j-2] \
-                     - 4.  * x[i-1, j-1] * h[i+1, j-1] \
-                     + 0.5 * x[i-1, j-1] * h[i+1, j  ] \
-                     + 4.  * x[i-1, j-1] * h[i,   j-1] \
-                     - 0.5 * x[i-1, j+1] * h[i-1, j+2] \
-                     + 0.5 * x[i-1, j+1] * h[i-1, j  ] \
-                     + 4.  * x[i-1, j+1] * h[i+1, j+1] \
-                     + 0.5 * x[i-1, j+1] * h[i+1, j+2] \
-                     - 0.5 * x[i-1, j+1] * h[i+1, j  ] \
-                     - 4.  * x[i-1, j+1] * h[i,   j+1] \
-                     + 0.5 * x[i-1, j+2] * h[i-1, j+1] \
-                     + 0.5 * x[i-1, j+2] * h[i+1, j+1] \
-                     - 1.  * x[i-1, j+2] * h[i,   j+1] \
-                     - 1.  * x[i-1, j  ] * h[i-2, j-1] \
-                     + 1.  * x[i-1, j  ] * h[i-2, j+1] \
-                     + 0.5 * x[i-1, j  ] * h[i-1, j-1] \
-                     - 0.5 * x[i-1, j  ] * h[i-1, j+1] \
-                     + 0.5 * x[i-1, j  ] * h[i+1, j-1] \
-                     - 0.5 * x[i-1, j  ] * h[i+1, j+1] \
-                     + 0.5 * x[i+1, j-2] * h[i-1, j-1] \
-                     + 0.5 * x[i+1, j-2] * h[i+1, j-1] \
-                     - 1.  * x[i+1, j-2] * h[i,   j-1] \
-                     + 0.5 * x[i+1, j-1] * h[i-1, j-2] \
-                     + 4.  * x[i+1, j-1] * h[i-1, j-1] \
-                     - 0.5 * x[i+1, j-1] * h[i-1, j  ] \
-                     - 0.5 * x[i+1, j-1] * h[i+1, j-2] \
-                     + 0.5 * x[i+1, j-1] * h[i+1, j  ] \
-                     - 4.  * x[i+1, j-1] * h[i,   j-1] \
-                     - 4.  * x[i+1, j+1] * h[i-1, j+1] \
-                     - 0.5 * x[i+1, j+1] * h[i-1, j+2] \
-                     + 0.5 * x[i+1, j+1] * h[i-1, j  ] \
-                     + 0.5 * x[i+1, j+1] * h[i+1, j+2] \
-                     - 0.5 * x[i+1, j+1] * h[i+1, j  ] \
-                     + 4.  * x[i+1, j+1] * h[i,   j+1] \
-                     - 0.5 * x[i+1, j+2] * h[i-1, j+1] \
-                     - 0.5 * x[i+1, j+2] * h[i+1, j+1] \
-                     + 1.  * x[i+1, j+2] * h[i,   j+1] \
-                     - 0.5 * x[i+1, j  ] * h[i-1, j-1] \
-                     + 0.5 * x[i+1, j  ] * h[i-1, j+1] \
-                     - 0.5 * x[i+1, j  ] * h[i+1, j-1] \
-                     + 0.5 * x[i+1, j  ] * h[i+1, j+1] \
-                     + 1.  * x[i+1, j  ] * h[i+2, j-1] \
-                     - 1.  * x[i+1, j  ] * h[i+2, j+1] \
-                     - 1.  * x[i+2, j-1] * h[i+1, j  ] \
-                     + 1.  * x[i+2, j-1] * h[i+2, j  ] \
-                     + 1.  * x[i+2, j+1] * h[i+1, j  ] \
-                     - 1.  * x[i+2, j+1] * h[i+2, j  ] \
-                     - 1.  * x[i+2, j  ] * h[i+2, j-1] \
-                     + 1.  * x[i+2, j  ] * h[i+2, j+1] \
-                     + 1.  * x[i+2, j  ] * h[i,   j-1] \
-                     - 1.  * x[i+2, j  ] * h[i,   j+1] \
-                     + 1.  * x[i,   j-1] * h[i-2, j  ] \
-                     - 1.  * x[i,   j-1] * h[i-1, j-2] \
-                     - 4.  * x[i,   j-1] * h[i-1, j-1] \
-                     + 1.  * x[i,   j-1] * h[i+1, j-2] \
-                     + 4.  * x[i,   j-1] * h[i+1, j-1] \
-                     - 1.  * x[i,   j-1] * h[i+2, j  ] \
-                     - 1.  * x[i,   j+1] * h[i-2, j  ] \
-                     + 4.  * x[i,   j+1] * h[i-1, j+1] \
-                     + 1.  * x[i,   j+1] * h[i-1, j+2] \
-                     - 4.  * x[i,   j+1] * h[i+1, j+1] \
-                     - 1.  * x[i,   j+1] * h[i+1, j+2] \
-                     + 1.  * x[i,   j+1] * h[i+2, j  ] \
-                 ) * self.hx_inv**3 * self.hy_inv / 24.
- 
-        return result
-    
     
     @cython.boundscheck(False)
-    cdef np.float64_t laplace(self, np.ndarray[np.float64_t, ndim=2] x,
+    cdef double laplace(self, double[:,:] x,
                                     np.uint64_t i, np.uint64_t j):
         
         cdef np.float64_t result
@@ -262,40 +92,6 @@ cdef class PETScDerivatives(object):
                    - 2. * x[i, j  ] \
                    + 1. * x[i, j+1] \
                  ) * self.hy_inv**2
- 
-        return result
-    
-
-    @cython.boundscheck(False)
-    cdef np.float64_t laplace_ave(self, np.ndarray[np.float64_t, ndim=2] x,
-                                        np.uint64_t i, np.uint64_t j):
-        
-        cdef np.float64_t result
-        
-        result = 0.25 * ( \
-                 ( \
-                   + 1. * x[i-1, j-1] \
-                   - 2. * x[i,   j-1] \
-                   + 1. * x[i+1, j-1] \
-                   + 2. * x[i-1, j  ] \
-                   - 4. * x[i,   j  ] \
-                   + 2. * x[i+1, j  ] \
-                   + 1. * x[i-1, j+1] \
-                   - 2. * x[i,   j+1] \
-                   + 1. * x[i+1, j+1] \
-                 ) * self.hx_inv**2 \
-               + ( \
-                   + 1. * x[i-1, j-1] \
-                   - 2. * x[i-1, j  ] \
-                   + 1. * x[i-1, j+1] \
-                   + 2. * x[i,   j-1] \
-                   - 4. * x[i,   j  ] \
-                   + 2. * x[i,   j+1] \
-                   + 1. * x[i+1, j-1] \
-                   - 2. * x[i+1, j  ] \
-                   + 1. * x[i+1, j+1] \
-                 ) * self.hy_inv**2 \
-               )
  
         return result
     
@@ -325,12 +121,11 @@ cdef class PETScDerivatives(object):
                 iy = i-xs
                 
                 ty[iy, jy] = sign * self.laplace(tx, ix, jx)
-#                 ty[iy, jy] = sign * self.laplace_ave(tx, ix, jx)
     
     
     
     @cython.boundscheck(False)
-    cpdef np.float64_t dx(self, Vec X, Vec Y, np.float64_t sign):
+    cpdef double dx(self, Vec X, Vec Y, np.float64_t sign):
     
         cdef np.uint64_t ix, iy, jx, jy, i, j
         cdef np.uint64_t xs, xe, ys, ye
@@ -353,19 +148,11 @@ cdef class PETScDerivatives(object):
                 ix = i-xs+2
                 iy = i-xs
                 
-#                 ty[iy, jy] = sign * (tx[ix+1, jx] - tx[ix, jx]) * self.hx_inv
-    
                 ty[iy, jy] = sign * (tx[ix+1, jx] - tx[ix-1, jx]) * 0.5 * self.hx_inv
     
-#                 ty[iy, jy] = sign * 0.25 * ( \
-#                                         + 1. * (tx[ix+1, jx-1] - tx[ix-1, jx-1]) \
-#                                         + 2. * (tx[ix+1, jx  ] - tx[ix-1, jx  ]) \
-#                                         + 1. * (tx[ix+1, jx+1] - tx[ix-1, jx+1]) \
-#                                       ) * 0.5 * self.hx_inv
-        
     
     @cython.boundscheck(False)
-    cpdef np.float64_t dy(self, Vec X, Vec Y, np.float64_t sign):
+    cpdef double dy(self, Vec X, Vec Y, np.float64_t sign):
     
         cdef np.uint64_t ix, iy, jx, jy, i, j
         cdef np.uint64_t xs, xe, ys, ye
@@ -388,82 +175,16 @@ cdef class PETScDerivatives(object):
                 ix = i-xs+2
                 iy = i-xs
                 
-#                 ty[iy, jy] = sign * (tx[ix, jx+1] - tx[ix, jx]) * self.hy_inv
-    
                 ty[iy, jy] = sign * (tx[ix, jx+1] - tx[ix, jx-1]) * 0.5 * self.hy_inv
-    
-#                 ty[iy, jy] = sign * 0.25 * ( \
-#                                         + 1. * (tx[ix-1, jx+1] - tx[ix-1, jx-1]) \
-#                                         + 2. * (tx[ix,   jx+1] - tx[ix,   jx-1]) \
-#                                         + 1. * (tx[ix+1, jx+1] - tx[ix+1, jx-1]) \
-#                                       ) * 0.5 * self.hy_inv
     
     
     @cython.boundscheck(False)
-    cdef np.float64_t dt(self, np.ndarray[np.float64_t, ndim=2] x,
+    cdef double dt(self, double[:,:] x,
                                np.uint64_t i, np.uint64_t j):
         '''
         Time Derivative
         '''
         
-        return self.average(x, i, j) * self.ht_inv
+        return x[i, j] * self.ht_inv
     
     
-    @cython.boundscheck(False)
-    cdef np.float64_t average(self, np.ndarray[np.float64_t, ndim=2] x,
-                                    np.uint64_t i, np.uint64_t j):
-        '''
-        Average
-        '''
-        
-        cdef np.float64_t result
-        
-        result = ( \
-                   + 1. * x[i-1, j-1] \
-                   + 2. * x[i-1, j  ] \
-                   + 1. * x[i-1, j+1] \
-                   + 2. * x[i,   j-1] \
-                   + 4. * x[i,   j  ] \
-                   + 2. * x[i,   j+1] \
-                   + 1. * x[i+1, j-1] \
-                   + 2. * x[i+1, j  ] \
-                   + 1. * x[i+1, j+1] \
-                 ) / 16.
-        
-        return result
-
-
-    @cython.boundscheck(False)
-    cdef np.float64_t dt_laplace(self, np.ndarray[np.float64_t, ndim=2] x,
-                                       np.uint64_t i, np.uint64_t j):
-        
-        cdef np.float64_t result
-        
-        result = 0.25 * ( \
-                 ( \
-                   + 1. * x[i-1, j-1] \
-                   - 2. * x[i,   j-1] \
-                   + 1. * x[i+1, j-1] \
-                   + 2. * x[i-1, j  ] \
-                   - 4. * x[i,   j  ] \
-                   + 2. * x[i+1, j  ] \
-                   + 1. * x[i-1, j+1] \
-                   - 2. * x[i,   j+1] \
-                   + 1. * x[i+1, j+1] \
-                 ) * self.hx_inv**2 * self.ht_inv \
-               + ( \
-                   + 1. * x[i-1, j-1] \
-                   - 2. * x[i-1, j  ] \
-                   + 1. * x[i-1, j+1] \
-                   + 2. * x[i,   j-1] \
-                   - 4. * x[i,   j  ] \
-                   + 2. * x[i,   j+1] \
-                   + 1. * x[i+1, j-1] \
-                   - 2. * x[i+1, j  ] \
-                   + 1. * x[i+1, j+1] \
-                 ) * self.hy_inv**2 * self.ht_inv \
-               )
- 
-        return result
-    
-
