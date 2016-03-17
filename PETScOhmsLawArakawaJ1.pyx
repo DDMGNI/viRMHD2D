46dd222eb1e1ef4ea46195f938bff85c6239b112
Michael Kraus <michael.kraus@ipp.mpg.de>
2016-03-17 12:00:08 +0100

    Cleanup and optimisations in Ohm's law solver.

--- PETScOhmsLawArakawaJ1.pyx
+++ PETScOhmsLawArakawaJ1.pyx
@@ -22,7 +22,7 @@ cdef class PETScOhmsLaw(object):
     built on top of the SciPy Sparse package.
     '''
     
-    def __init__(self, object da1, np.uint64_t nx, np.uint64_t ny,
+    def __init__(self, object da1, int nx, int ny,
                        double ht, double hx, double hy):
         '''
         Constructor
@@ -43,6 +43,7 @@ cdef class PETScOhmsLaw(object):
         self.hx_inv = 1. / hx
         self.hy_inv = 1. / hy
         
+        self.arakawa_fac = 0.5 * self.ht * self.hx_inv * self.hy_inv / 12.
         
         # create history vector
         self.Ah = self.da1.createGlobalVec()
@@ -65,19 +66,16 @@ cdef class PETScOhmsLaw(object):
     
     @cython.boundscheck(False)
     def formMat(self, Mat A):
-        cdef np.int64_t i, j
-        cdef np.int64_t ix, iy, jx, jy
-        cdef np.int64_t xe, xs, ye, ys
+        cdef int i, j, stencil
+        cdef int ix, iy, jx, jy
+        cdef int xe, xs, ye, ys
         
         (xs, xe), (ys, ye) = self.da1.getRanges()
+        stencil = self.da1.getStencilWidth()
         
         self.da1.globalToLocal(self.Ph, self.localPh)
         
-        cdef np.ndarray[np.float64_t, ndim=2] Ph = self.da1.getVecArray(self.localPh)[...]
-        
-        cdef double[:,:] P_ave = Ph
-        
-        cdef double arak_fac = 0.5 * self.hx_inv * self.hy_inv / 12.
+        cdef double[:,:] P_ave = self.da1.getVecArray(self.localPh)[...]
         
         
         A.zeroEntries()
@@ -89,28 +87,28 @@ cdef class PETScOhmsLaw(object):
         col.field = 0
         
         for i in range(xs, xe):
-            ix = i-xs+2
+            ix = i-xs+stencil
             
             for j in range(ys, ye):
-                jx = j-ys+2
+                jx = j-ys+stencil
                 
                 row.index = (i,j)
                 
                 # dA/dt + [P, dA] 
                 for index, value in [
-                        ((i-1, j-1), + (P_ave[ix-1, jx  ] - P_ave[ix,   jx-1]) * arak_fac),
-                        ((i-1, j  ), + (P_ave[ix,   jx+1] - P_ave[ix,   jx-1]) * arak_fac \
-                                     + (P_ave[ix-1, jx+1] - P_ave[ix-1, jx-1]) * arak_fac),
-                        ((i-1, j+1), + (P_ave[ix,   jx+1] - P_ave[ix-1, jx  ]) * arak_fac),
-                        ((i,   j-1), - (P_ave[ix+1, jx  ] - P_ave[ix-1, jx  ]) * arak_fac \
-                                     - (P_ave[ix+1, jx-1] - P_ave[ix-1, jx-1]) * arak_fac),
+                        ((i-1, j-1), + (P_ave[ix-1, jx  ] - P_ave[ix,   jx-1]) * self.arakawa_fac),
+                        ((i-1, j  ), + (P_ave[ix,   jx+1] - P_ave[ix,   jx-1]) * self.arakawa_fac \
+                                     + (P_ave[ix-1, jx+1] - P_ave[ix-1, jx-1]) * self.arakawa_fac),
+                        ((i-1, j+1), + (P_ave[ix,   jx+1] - P_ave[ix-1, jx  ]) * self.arakawa_fac),
+                        ((i,   j-1), - (P_ave[ix+1, jx  ] - P_ave[ix-1, jx  ]) * self.arakawa_fac \
+                                     - (P_ave[ix+1, jx-1] - P_ave[ix-1, jx-1]) * self.arakawa_fac),
                         ((i,   j  ), self.ht_inv),
-                        ((i,   j+1), + (P_ave[ix+1, jx  ] - P_ave[ix-1, jx  ]) * arak_fac \
-                                     + (P_ave[ix+1, jx+1] - P_ave[ix-1, jx+1]) * arak_fac),
-                        ((i+1, j-1), - (P_ave[ix+1, jx  ] - P_ave[ix,   jx-1]) * arak_fac),
-                        ((i+1, j  ), - (P_ave[ix,   jx+1] - P_ave[ix,   jx-1]) * arak_fac \
-                                     - (P_ave[ix+1, jx+1] - P_ave[ix+1, jx-1]) * arak_fac),
-                        ((i+1, j+1), - (P_ave[ix,   jx+1] - P_ave[ix+1, jx  ]) * arak_fac),
+                        ((i,   j+1), + (P_ave[ix+1, jx  ] - P_ave[ix-1, jx  ]) * self.arakawa_fac \
+                                     + (P_ave[ix+1, jx+1] - P_ave[ix-1, jx+1]) * self.arakawa_fac),
+                        ((i+1, j-1), - (P_ave[ix+1, jx  ] - P_ave[ix,   jx-1]) * self.arakawa_fac),
+                        ((i+1, j  ), - (P_ave[ix,   jx+1] - P_ave[ix,   jx-1]) * self.arakawa_fac \
+                                     - (P_ave[ix+1, jx+1] - P_ave[ix+1, jx-1]) * self.arakawa_fac),
+                        ((i+1, j+1), - (P_ave[ix,   jx+1] - P_ave[ix+1, jx  ]) * self.arakawa_fac),
                     ]:
   
                     col.index = index
@@ -126,32 +124,31 @@ cdef class PETScOhmsLaw(object):
         
     
     def function(self, Vec X, Vec Y):
-        cdef np.int64_t i, j
-        cdef np.int64_t ix, iy, jx, jy
-        cdef np.int64_t xe, xs, ye, ys
+        cdef int i, j, stencil
+        cdef int ix, iy, jx, jy
+        cdef int xe, xs, ye, ys
         
         (xs, xe), (ys, ye) = self.da1.getRanges()
+        stencil = self.da1.getStencilWidth()
         
         self.da1.globalToLocal(X,       self.localAp)
         self.da1.globalToLocal(self.Ah, self.localAh)
         self.da1.globalToLocal(self.Ph, self.localPh)
         
-        cdef np.ndarray[np.float64_t, ndim=2] Ap = self.da1.getVecArray(self.localAp)[...]
-        cdef np.ndarray[np.float64_t, ndim=2] Ah = self.da1.getVecArray(self.localAh)[...]
-        cdef np.ndarray[np.float64_t, ndim=2] Ph = self.da1.getVecArray(self.localPh)[...]
+        cdef double[:,:] Ah = self.da1.getVecArray(self.localAh)[...]
+        cdef double[:,:] Ap = self.da1.getVecArray(self.localAp)[...]
         
-        cdef double[:,:] A_ave = 0.5 * (Ap + Ah)
-        cdef double[:,:] P_ave = Ph
-        
+        cdef double[:,:] A_ave = 0.5 * (self.da1.getVecArray(self.localAp)[:,:] + self.da1.getVecArray(self.localAh)[:,:])
+        cdef double[:,:] P_ave = self.da1.getVecArray(self.localPh)[...]
         cdef double[:,:] y     = self.da1.getVecArray(Y)[...]
         
         
         for i in range(xs, xe):
-            ix = i-xs+2
+            ix = i-xs+stencil
             iy = i-xs
             
             for j in range(ys, ye):
-                jx = j-ys+2
+                jx = j-ys+stencil
                 jy = j-ys
                 
                 y[iy, jy] = \
