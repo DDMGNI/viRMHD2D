c76408194ebd68a7bdd41231de03ad36e7a6c6a4
Michael Kraus <michael.kraus@ipp.mpg.de>
2016-03-21 09:31:44 +0100

    Simplified setup.py and added flags for profiling.

--- setup.py
+++ setup.py
@@ -5,15 +5,33 @@
 from distutils.core      import setup
 from distutils.extension import Extension
 from Cython.Distutils    import build_ext
+from Cython.Build        import cythonize
+from Cython.Compiler.Options import directive_defaults
 
 import os
 from os.path import join, isdir
 
+# os.environ["CC"]  = "gcc-mp-5"
+# os.environ["CXX"] = "g++-mp-5"
+
+# os.environ["CC"]  = "clang"
+# os.environ["CXX"] = "clang++"
+
+
 INCLUDE_DIRS = []
 LIBRARY_DIRS = []
 LIBRARIES    = []
-CARGS        = ['-O3', '-axavx', '-march=corei7-avx', '-std=c99', '-Wno-unused-function', '-Wno-unneeded-internal-declaration']
+CARGS        = ['-O3', '-march=native', '-std=c99', '-Wno-unused-function', '-Wno-unneeded-internal-declaration']
+#CARGS       += ['-Wa,-q'] # needed by some versions of gcc on MacOSX when march is set to native
 LARGS        = []
+MACROS       = []
+# MACROS       = [('CYTHON_TRACE', '1')]
+
+# profiling
+# directive_defaults['profile']   = True
+# directive_defaults['linetrace'] = True
+# directive_defaults['binding']   = True
+
 
 # PETSc
 PETSC_DIR  = os.environ['PETSC_DIR']
@@ -51,74 +69,29 @@ if isdir('/opt/local/lib/openmpi-gcc5'):
     LIBRARY_DIRS += ['/opt/local/lib/openmpi-gcc5']
 
 
+extension_list = ["PETScDerivatives",
+                  "PETScNonlinearSolverArakawaJ1CFD2",
+                  "PETScPreconditionerArakawaJ1CFD2",
+                  "PETScPreconditionerArakawaJ1CFD2Vec",
+                  "PETScOhmsLawArakawaJ1",
+                  "PETScPoissonCFD2",
+                  "PETScVorticityArakawaJ1"]
+
+
 ext_modules = [
-        Extension("PETScDerivatives",
-                  sources=["PETScDerivatives.pyx"],
+        Extension(ext,
+                  sources=[ext + ".pyx"],
                   include_dirs=INCLUDE_DIRS + [os.curdir],
                   libraries=LIBRARIES,
                   library_dirs=LIBRARY_DIRS,
                   runtime_library_dirs=LIBRARY_DIRS,
                   extra_compile_args=CARGS,
-                  extra_link_args=LARGS
-                 ),
-        Extension("PETScNonlinearSolverArakawaJ1CFD2",
-                  sources=["PETScNonlinearSolverArakawaJ1CFD2.pyx"],
-                  include_dirs=INCLUDE_DIRS + [os.curdir],
-                  libraries=LIBRARIES,
-                  library_dirs=LIBRARY_DIRS,
-                  runtime_library_dirs=LIBRARY_DIRS,
-                  extra_compile_args=CARGS,
-                  extra_link_args=LARGS
-                 ),
-        Extension("PETScPreconditionerArakawaJ1CFD2",
-                  sources=["PETScPreconditionerArakawaJ1CFD2.pyx"],
-                  include_dirs=INCLUDE_DIRS + [os.curdir],
-                  libraries=LIBRARIES,
-                  library_dirs=LIBRARY_DIRS,
-                  runtime_library_dirs=LIBRARY_DIRS,
-                  extra_compile_args=CARGS,
-                  extra_link_args=LARGS
-                 ),
-        Extension("PETScPreconditionerArakawaJ1CFD2Vec",
-                  sources=["PETScPreconditionerArakawaJ1CFD2Vec.pyx"],
-                  include_dirs=INCLUDE_DIRS + [os.curdir],
-                  libraries=LIBRARIES,
-                  library_dirs=LIBRARY_DIRS,
-                  runtime_library_dirs=LIBRARY_DIRS,
-                  extra_compile_args=CARGS,
-                  extra_link_args=LARGS
-                 ),
-        Extension("PETScOhmsLawArakawaJ1",
-                  sources=["PETScOhmsLawArakawaJ1.pyx"],
-                  include_dirs=INCLUDE_DIRS + [os.curdir],
-                  libraries=LIBRARIES,
-                  library_dirs=LIBRARY_DIRS,
-                  runtime_library_dirs=LIBRARY_DIRS,
-                  extra_compile_args=CARGS,
-                  extra_link_args=LARGS
-                 ),
-        Extension("PETScPoissonCFD2",
-                  sources=["PETScPoissonCFD2.pyx"],
-                  include_dirs=INCLUDE_DIRS + [os.curdir],
-                  libraries=LIBRARIES,
-                  library_dirs=LIBRARY_DIRS,
-                  runtime_library_dirs=LIBRARY_DIRS,
-                  extra_compile_args=CARGS,
-                  extra_link_args=LARGS
-                 ),
-        Extension("PETScVorticityArakawaJ1",
-                  sources=["PETScVorticityArakawaJ1.pyx"],
-                  include_dirs=INCLUDE_DIRS + [os.curdir],
-                  libraries=LIBRARIES,
-                  library_dirs=LIBRARY_DIRS,
-                  runtime_library_dirs=LIBRARY_DIRS,
-                  extra_compile_args=CARGS,
-                  extra_link_args=LARGS
-                 )
-              ]
+                  extra_link_args=LARGS,
+                  define_macros=MACROS
+                 ) for ext in extension_list]
                 
 setup(
-    name = 'PETSc MHD Solver',
+    name = 'PETSc RMHD Solver',
     cmdclass = {'build_ext': build_ext},
     ext_modules = ext_modules
 )
