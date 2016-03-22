c171d349b5429d985acd70a5247a50f823e24634
Michael Kraus <michael.kraus@ipp.mpg.de>
2016-03-22 21:21:01 +0100

    Added reconnection run files.

---
+++ runs/reconnection.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+
+
+def magnetic_A(x, y, Lx, Ly):
+    return - 1.29 / np.cosh(x)**2
+
+def velocity_P(x, y, Lx, Ly):
+    return 1.E-3 * (np.cos(x+y) - np.cos(x-y))
+
+def current_perturbation(x, y, Lx, Ly):
+    return 0.
+
+def vorticity_perturbation(x, y, Lx, Ly):
+    return 0.
